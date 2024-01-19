bce173e6fb5a028efcde298ffde28b1f5c308ead
Archer Chang <archerchang@qnap.com>
2024-01-19 10:46:11 +0800

    rework

--- monitor/file/inotify/inotify.cpp
+++ monitor/file/inotify/inotify.cpp
@@ -12,9 +12,11 @@
 #include <limits.h>
 #include <string.h>
 #include <assert.h>
+#include <errno.h>
+
+#include <chrono>
 
 using namespace lkup69;
-
 
 /* Supported events suitable for MASK parameter of INOTIFY_ADD_WATCH.  */
 // IN_ACCESS             0x00000001     /* File was accessed.  */
@@ -50,7 +52,23 @@ using namespace lkup69;
 // IN_ISDIR              0x40000000 /* Event occurred against dir.  */
 // IN_ONESHOT            0x80000000     /* Only send event once.  */
 
-static void display_inotify_event(struct inotify_event *i)
+Inotify::~Inotify()
+{
+        if (m_bRuning)
+                m_bExit = true;
+
+        while (m_bExit)
+                std::this_thread::sleep_for(std::chrono::microseconds(100));
+
+        if (m_inotifyFd != -1)
+                close(m_inotifyFd);
+
+        if (m_epollFd != -1)
+                close(m_epollFd);
+}
+
+// static
+void Inotify::DumpInotifyEvent(struct inotify_event *i)
 {
         printf("    wd =%2d; ", i->wd);
         if (i->cookie > 0)
@@ -95,40 +113,31 @@ static void display_inotify_event(struct inotify_event *i)
                 printf("        name = %s\n", i->name);
 }
 
-Inotify::~Inotify()
-{
-        if (m_bRuning)
-                m_bExit = true;
-
-        while (m_bExit);
-
-        if (m_inotifyFd != -1)
-                close(m_inotifyFd);
-
-        if (m_epollFd != -1)
-                close(m_epollFd);
-}
-
 int Inotify::Init()
 {
         if ((m_inotifyFd = inotify_init()) < 0) {
                 fprintf(stderr,
-                        "[%s][%s][%d][err] initfy_init() \n",
-                        __FILE__, __PRETTY_FUNCTION__, __LINE__);
+                        "[%s][%s][%d][err] initfy_init() %s\n",
+                        __FILE__,
+                        __PRETTY_FUNCTION__,
+                        __LINE__,
+                        strerror(errno));
                 return -1;
         }
 
         if ((m_epollFd = epoll_create1(0)) < 0) {
                 fprintf(stderr,
-                        "[%s][%s][%d][err] epoll_create1() \n",
-                        __FILE__, __PRETTY_FUNCTION__, __LINE__);
-                if (m_inotifyFd != -1) {
-                        close(m_inotifyFd);
-                        m_inotifyFd = -1;
-                }
+                        "[%s][%s][%d][err] epoll_create1() %s\n",
+                        __FILE__,
+                        __PRETTY_FUNCTION__,
+                        __LINE__,
+                        strerror(errno));
+                close(m_inotifyFd);
+                m_inotifyFd = -1;
 
                 return -1;
         }
+
         return 0;
 }
 
@@ -141,8 +150,11 @@ int Inotify::AddWatch(const std::string &path, uint32_t interesting)
         if ((wd = inotify_add_watch(m_inotifyFd, path.c_str(), interesting)) == -1) {
                 fprintf(stderr,
                         "[%s][%s][%d][err] inotify_add_watch(%d, %s) \n",
-                        __FILE__, __PRETTY_FUNCTION__, __LINE__,
-                        m_inotifyFd, path.c_str());
+                        __FILE__,
+                        __PRETTY_FUNCTION__,
+                        __LINE__,
+                        m_inotifyFd,
+                        path.c_str());
 
                 return -1;
         }
@@ -163,8 +175,12 @@ int Inotify::RemoveWatch(const std::string &path)
                         if (inotify_rm_watch(m_inotifyFd, it->first) < 0) {
                                 fprintf(stderr,
                                         "[%s][%s][%d][err] inotify_rm_watch(%d, %d) :%s \n",
-                                        __FILE__, __PRETTY_FUNCTION__, __LINE__,
-                                        m_inotifyFd, it->first, it->second.c_str());
+                                        __FILE__,
+                                        __PRETTY_FUNCTION__,
+                                        __LINE__,
+                                        m_inotifyFd,
+                                        it->first,
+                                        it->second.c_str());
 
                                 return -1;
                         }
@@ -176,7 +192,9 @@ int Inotify::RemoveWatch(const std::string &path)
 
         fprintf(stderr,
                 "[%s][%s][%d][err] not exist: %s \n",
-                __FILE__, __PRETTY_FUNCTION__, __LINE__,
+                __FILE__,
+                __PRETTY_FUNCTION__,
+                __LINE__,
                 path.c_str());
 
         return -1;
@@ -184,49 +202,60 @@ int Inotify::RemoveWatch(const std::string &path)
 
 void Inotify::RegisterObserver(InotifyObserver *obs)
 {
-        m_observer = obs;
+        m_observerVec.push_back(obs);
 }
 
 void Inotify::Start(void)
 {
-        assert(m_observer != nullptr);
-
         m_thread.reset(new std::thread(&Inotify::Watcher_, this));
         m_thread->detach();
 }
 
-#define BUF_LEN (10 * (sizeof(struct inotify_event) + NAME_MAX + 1))
+#define BUF_LEN         (10 * (sizeof(struct inotify_event) + NAME_MAX + 1))
 #define EPOLL_EVENT_LEN 1
 
 void Inotify::Watcher_(void)
 {
-        struct inotify_event *event;
-        struct epoll_event   ev;
-        struct epoll_event   events[EPOLL_EVENT_LEN];
-        char                 *p;
-        int                  numRead;
-        int                  nfds;
+        struct epoll_event events[EPOLL_EVENT_LEN] = { 0 };
 
+        {
+                struct epoll_event ev;
 
-        ev.events = EPOLLIN;
-        ev.data.fd = m_inotifyFd;
-        if (epoll_ctl(m_epollFd, EPOLL_CTL_ADD, m_inotifyFd, &ev) == -1) {
-                fprintf(stderr,
-                        "[%s][%s][%d][err] epoll_ctl() \n",
-                        __FILE__, __PRETTY_FUNCTION__, __LINE__);
-                return;
+                ev.events  = EPOLLIN;
+                ev.data.fd = m_inotifyFd;
+                if (epoll_ctl(m_epollFd, EPOLL_CTL_ADD, m_inotifyFd, &ev) == -1) {
+                        fprintf(stderr,
+                                "[%s][%s][%d][err] epoll_ctl() %s\n",
+                                __FILE__,
+                                __PRETTY_FUNCTION__,
+                                __LINE__,
+                                strerror(errno));
+                        return;
+                }
         }
 
         m_bRuning = true;
         while (!m_bExit) {
-                char buf[BUF_LEN] = {0};
+                int nfds = epoll_wait(m_epollFd, events, 1, 1000);  // 1sec
 
-                nfds = epoll_wait(m_epollFd, events, 1, 1000); // 1sec
+                if (nfds < 0) {
+                        if (errno == EINTR)
+                                continue;
 
-                if ((nfds < 0) || m_bExit)
-                        continue;
+                        fprintf(stderr,
+                                "[%s][%s][%d][err] epoll_wait() ... %s \n",
+                                __FILE__,
+                                __PRETTY_FUNCTION__,
+                                __LINE__,
+                                strerror(errno));
+                        break;
+                }
 
                 if (events[0].data.fd == m_inotifyFd) {
+                        char  buf[BUF_LEN + 1] = { 0 };
+                        char *p                = nullptr;
+                        int   numRead          = 0;
+
                         /* Read events forever */
                         numRead = read(m_inotifyFd, buf, BUF_LEN);
                         if (numRead == 0)
@@ -234,17 +263,25 @@ void Inotify::Watcher_(void)
 
                         if (numRead == -1) {
                                 fprintf(stderr,
-                                        "[%s][%s][%d][err] read() \n",
-                                        __FILE__, __PRETTY_FUNCTION__, __LINE__);
+                                        "[%s][%s][%d][err] read() %s\n",
+                                        __FILE__,
+                                        __PRETTY_FUNCTION__,
+                                        __LINE__,
+                                        strerror(errno));
                                 continue;
                         }
 
-                        //printf("Read %ld bytes from inotify fd\n", (long)numRead);
+                        // printf("Read %ld bytes from inotify fd\n", (long)numRead);
                         for (p = buf; p < (buf + numRead);) {
+                                struct inotify_event *event;
+
                                 event = (struct inotify_event *)p;
 
-                                if (m_observer)
-                                        m_observer->OnInotifyEvent(this, event, m_watchMap[event->wd]);
+                                // Debug
+                                Inotify::DumpInotifyEvent(event);
+
+                                for (auto &it : m_observerVec)
+                                        it->OnInotifyEvent(this, event, m_watchMap[event->wd]);
 
                                 p += sizeof(struct inotify_event) + event->len;
                         }
@@ -252,7 +289,7 @@ void Inotify::Watcher_(void)
         }
 
         m_bRuning = false;
-        m_bExit = false;
+        m_bExit   = false;
 }
 
 // g++ -Wall -DUNIT_TEST -std=c++17 -o inotify inotify.cpp -pthread
@@ -263,7 +300,7 @@ class UnitTest : public InotifyObserver
 public:
         int OnInotifyEvent(Inotify *inotify, struct inotify_event *event, const std::string &path) override
         {
-                display_inotify_event(event);
+                Inotify::DumpInotifyEvent(event);
                 printf(">>>>>> %s\n", path.c_str());
                 return 0;
         }
@@ -299,14 +336,14 @@ int main(int argc, char *argv[])
 
                 // Test Remove
                 printf("Test 2 remove:%s.....\n", argv[1]);
-                in->RemoveWatch(argv[1]);  
-                cnt = 20;                      
+                in->RemoveWatch(argv[1]);
+                cnt = 20;
                 while (--cnt)
                         sleep(1);
 
                 // Test Add
                 printf("Test 3 re add:%s.....\n", argv[1]);
-                in->AddWatch(argv[1]);                        
+                in->AddWatch(argv[1]);
                 cnt = 20;
                 while (--cnt)
                         sleep(1);
--- monitor/file/inotify/inotify.h
+++ monitor/file/inotify/inotify.h
@@ -7,19 +7,20 @@
 #include <map>
 #include <thread>
 #include <memory>
+#include <vector>
 
-namespace lkup69 {
+namespace lkup69
+{
 
 class Inotify;
 class InotifyObserver
 {
-
 public:
-        InotifyObserver() = default;
+        InotifyObserver()          = default;
         virtual ~InotifyObserver() = default;
 
-        InotifyObserver(const InotifyObserver &) = delete;
-        InotifyObserver &operator= (const InotifyObserver &) = delete;
+        InotifyObserver(const InotifyObserver &)            = delete;
+        InotifyObserver &operator=(const InotifyObserver &) = delete;
 
         virtual int OnInotifyEvent(Inotify *inotify, struct inotify_event *event, const std::string &path) = 0;
 };
@@ -30,24 +31,29 @@ public:
         Inotify() = default;
         virtual ~Inotify();
 
-        Inotify(const Inotify &) = delete;
+        Inotify(const Inotify &)            = delete;
         Inotify &operator=(const Inotify &) = delete;
 
-        int Init();
-        int AddWatch(const std::string &path, uint32_t interesting = IN_ALL_EVENTS);
-        int RemoveWatch(const std::string &path);
+public:
+        static void DumpInotifyEvent(struct inotify_event *i);
+
+public:
+        int  Init();
+        int  AddWatch(const std::string &path, uint32_t interesting = IN_ALL_EVENTS);
+        int  RemoveWatch(const std::string &path);
         void RegisterObserver(InotifyObserver *obs);
         void Start(void);
-private:
-        void Watcher_(void);       
 
 private:
-        int                           m_inotifyFd = -1;
-        int                           m_epollFd = -1;
-        bool                          m_bExit = false;
-        bool                          m_bRuning = false;
-        InotifyObserver               *m_observer = nullptr;
-        std::map<int, std::string>    m_watchMap;
-        std::unique_ptr<std::thread>  m_thread;
+        void Watcher_(void);
+
+private:
+        int                            m_inotifyFd = -1;
+        int                            m_epollFd   = -1;
+        volatile bool                  m_bExit     = false;
+        volatile bool                  m_bRuning   = false;
+        std::vector<InotifyObserver *> m_observerVec;
+        std::map<int, std::string>     m_watchMap;
+        std::unique_ptr<std::thread>   m_thread;
 };
-}
+}  // namespace lkup69
