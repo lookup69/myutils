198339b1ee8b2981a60e235d9ed37dadd70b353b
Archer Chang <archerchang@qnap.com>
2024-01-19 17:26:55 +0800

    Add DupToStdout() to redirect stdout to Pipe
                          fork()
    ipc.BeWriter();                                  ipc.BeReader();
    ipc.DupToStdout();
    exec(command) ---- stdout redirect to Pipe ----> ipc.Read()

--- ipc/pipeIpc.cpp
+++ ipc/pipeIpc.cpp
@@ -4,12 +4,11 @@ using namespace lkup69;
 
 PipeIpc::~PipeIpc()
 {
-        if(m_fd[0] != -1)
+        if (m_fd[0] != -1)
                 close(m_fd[0]);
-        if(m_fd[1] != -1)
+        if (m_fd[1] != -1)
                 close(m_fd[1]);
 }
-
 
 int PipeIpc::Init(void)
 {
@@ -18,7 +17,7 @@ int PipeIpc::Init(void)
 
 void PipeIpc::BeReader(void)
 {
-        if(m_fd[1] != -1) {
+        if (m_fd[1] != -1) {
                 close(m_fd[1]);
                 m_fd[1] = -1;
         }
@@ -26,7 +25,7 @@ void PipeIpc::BeReader(void)
 
 void PipeIpc::BeWriter(void)
 {
-        if(m_fd[0] != -1) {
+        if (m_fd[0] != -1) {
                 close(m_fd[0]);
                 m_fd[0] = -1;
         }
@@ -42,38 +41,44 @@ int PipeIpc::Write(char *buf, size_t size)
         return write(m_fd[1], buf, size);
 }
 
+int PipeIpc::DupToStdout()
+{
+        //return dup2(STDOUT_FILENO, m_fd[1]);
+        return dup2(m_fd[1], STDOUT_FILENO);
+}
+
 #ifdef UNIT_TEST
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
-//g++ -std=c++11 -DUNIT_TEST pipeIpc pipeIpc.cpp
+// g++ -std=c++11 -DUNIT_TEST pipeIpc pipeIpc.cpp
 int main()
 {
         PipeIpc ipc;
         pid_t   pid;
 
-        if(ipc.Init() < 0) {
+        if (ipc.Init() < 0) {
                 printf("ipc.Init() ... fail\n");
                 exit(0);
         }
 
         pid = fork();
-        if(pid == 0) {
+        if (pid == 0) {
                 ipc.BeReader();
 
-                for(int i = 0; i < 5; ++i) {
-                        char buf[128] = {0};
+                for (int i = 0; i < 5; ++i) {
+                        char buf[128] = { 0 };
 
                         ipc.Read(buf, sizeof(buf));
                         printf(">>>Reader: %s\n", buf);
                 }
         }
-        if(pid > 0) {
+        if (pid > 0) {
                 ipc.BeWriter();
 
-                for(int i = 0; i < 5; ++i) {
-                        char buf[128] = {0};
+                for (int i = 0; i < 5; ++i) {
+                        char buf[128] = { 0 };
 
                         sprintf(buf, " Writer:%d", i);
                         ipc.Write(buf, strlen(buf));
@@ -83,6 +88,5 @@ int main()
         sleep(3);
 
         return 0;
-
 }
 #endif
--- ipc/pipeIpc.h
+++ ipc/pipeIpc.h
@@ -5,17 +5,20 @@ namespace lkup69 {
 
 class PipeIpc
 {
-        PipeIpc(const PipeIpc &) = delete;
-        PipeIpc &operator=(const PipeIpc &) = delete;
 public:
         PipeIpc() = default;
         ~PipeIpc();
+        PipeIpc(const PipeIpc &) = delete;
+        PipeIpc &operator=(const PipeIpc &) = delete;
 
+public:
         int Init(void);
         void BeWriter(void);
         void BeReader(void);
         int Read(char *buf, size_t size);
         int Write(char *buf, size_t size);
+        int DupToStdout();
+
 private:
         int m_fd[2] = {-1, -1};
 };
